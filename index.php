7121adfc6f53b2b84524de06245fd60b05621a3a
stevenroach7 <sroach@macalester.edu>
2016-06-30 23:09:38 -0500

    Git thought file was binary.

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 <Body>
 
   <p>
-    GroupStart is an experimental, alpha-stage application that seeks to facilitate the early stages of an online group project.
+    GroupStart is an experimental, alpha-stage application
+     that seeks to facilitate the early stages of an online group project.
   </p>
   <h5>University of Minnesota Twin Cities - Computer Science &amp; Engineering - GroupLens Research</h5>
 
