f0e269d261e874c762aeb67fef1a1b491986d306
stevenroach7 <sroach@macalester.edu>
2016-07-07 16:29:29 -0500

    Setup project structure.

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<html>
+<?php
 
+
+
+
+ ?>
+
+
+<html>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+
+<script src="js/dropzone.min.js"></script>
 
 <head>
   <h1>GroupStart</h1>
 </head>
 
 
-<Body>
+<body>
 
   <p>
     GroupStart is an experimental, alpha-stage application
@@ -14,9 +25,21 @@
   </p>
   <h5>University of Minnesota Twin Cities - Computer Science &amp; Engineering - GroupLens Research</h5>
 
-</Body>
+  <a href="views/login.php">Login</a>
+
 
   <?php
     $name = "Steven";
-    echo "Welcome to GroupStart, ".$name.".";
+    $name1 = "Tosin";
+    echo "<h3>Welcome to GroupStart, ".$name." and ".$name1.".</h3>";
   ?>
+
+
+
+
+
+  <form action="../php_scripts/upload.php" class="dropzone"></form>
+
+
+
+</body>
---
+++ views/login.php
@@ -0,0 +1,48 @@
+<?php
+
+
+
+
+ ?>
+
+
+<html>
+<link rel="stylesheet" type="text/css" href="../css/style.css" />
+
+
+<head>
+  <h1>GroupStart</h1>
+</head>
+
+
+<body>
+
+  <p>
+    GroupStart is an experimental, alpha-stage application
+     that seeks to facilitate the early stages of an online group project.
+  </p>
+  <h5>University of Minnesota Twin Cities - Computer Science &amp; Engineering - GroupLens Research</h5>
+
+  <a href="#loginModal">Login</a>
+
+  <div id="loginModal" class="modalDialog">
+    <div>
+		  <a href="#close" title="Close" class="close">X</a>
+		  <h2>Login</h2>
+		  <p>This is a sample modal box that can be created using the powers of CSS3.</p>
+		  <p>You could do a lot of things here like have a pop-up ad that shows when your website loads, or create a login/register form for users.</p>
+	  </div>
+
+  </div>
+
+
+  <?php
+    $name = "Steven";
+    $name1 = "Tosin";
+    echo "<h3>Welcome to GroupStart, ".$name." and ".$name1.".</h3>";
+  ?>
+
+
+
+
+</body>
