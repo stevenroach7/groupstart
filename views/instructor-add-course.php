be989419e5ca0a57d48acf243783b4dd78d1e2cb
Tosin Alliyu <oballiyu@haverford.edu>
2016-07-08 14:09:25 -0500

    Instructor add course page wireframed

--- views/instructor-add-course.php
+++ views/instructor-add-course.php
@@ -14,6 +14,22 @@
 
 
   <body>
+      <h1>Add New Course</h1>
+      <div class=main-page-form>
+      <form id ="add-course-form">
+          <h4>Course Name</h4><input type="text" name="course-name">
+          
+          <h4>Short Course Description</h4><br>
+          <textarea rows="4" cols="50" name="course-description"></textarea><br>
+          
+          <h4>Course Attachments</h4>
+          
+      </form>
+           <form action="../php_scripts/upload.php" class="dropzone"></form>
+          <input type="submit" value="Add Course">
+          
+         
+          </div>
 
   </body>
 </html>
