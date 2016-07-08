c99eeb3c3e9dd8eb3d5df1c1929ceee489f426dc
stevenroach7 <sroach@macalester.edu>
2016-07-08 14:44:00 -0500

    Finished wireframe login page

--- views/login.php
+++ views/login.php
@@ -41,13 +41,19 @@
           <p><input type="text" name="email" maxlength="255" placeholder="Email"></p>
           <p><input type="text" name="password" maxlength="32" placeholder="Password"></p>
           <p><input type="text" name="confirm-password" maxlength="32" placeholder="Confirm Password"></p>
-          <p><input type="radio" name="age-range" value="0-18">18 or younger
+          <p><input type="radio" name="age-range" value="18-">0-18
             <input type="radio" name="age-range" value="19-29">19-29
             <input type="radio" name="age-range" value="30-39">30-39
-            <input type="radio" name="age-range" value="30-39">30-39
-
+            <input type="radio" name="age-range" value="40-49">40-49
+            <input type="radio" name="age-range" value="50-59">50-59
+            <input type="radio" name="age-range" value="60+">60+
           </p>
-          <p><input type="submit" name="sign-in" value="Sign In"></p>
+          <p>
+            <?php include 'features/timezones.php' ?>
+          </p>
+
+
+          <p><input type="submit" name="sign-in" value="Sign Up"></p>
         </form>
   	  </div>
     </div>
@@ -57,11 +63,19 @@
   		  <a href="#close" title="Close" class="close">close</a>
   		  <h2>Instructor Sign Up</h2>
         <form method="post" action="login.php">
-          <p><input type="radio" name="account-type" value="student">Student
-            <input type="radio" name="account-type" value="instructor">Instructor</p>
+          <p><input type="text" name="first-name" placeholder="First Name">
+            <input type="text" name="last-name" placeholder="Last Name">
+          </p>
+          <p><input type="text" name="display-name" maxlength="255" placeholder="Preferred Display Name"></p>
           <p><input type="text" name="email" maxlength="255" placeholder="Email"></p>
           <p><input type="text" name="password" maxlength="32" placeholder="Password"></p>
-          <p><input type="submit" name="sign-in" value="Sign In"></p>
+          <p><input type="text" name="confirm-password" maxlength="32" placeholder="Confirm Password"></p>
+          <p>
+            <?php include 'features/timezones.php' ?>
+          </p>
+
+
+          <p><input type="submit" name="sign-in" value="Sign Up"></p>
         </form>
   	  </div>
     </div>
