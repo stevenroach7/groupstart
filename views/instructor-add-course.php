43e47a5705652a9ffc207265c4a8a57de56e89f4
Tosin Alliyu <oballiyu@haverford.edu>
2016-07-19 10:50:50 -0500

    Connecting DB to Add courses

--- views/instructor-add-course.php
+++ views/instructor-add-course.php
@@ -6,6 +6,7 @@
       include 'features/authentication.php';
       include 'features/instructor-authentication.php';
       include 'features/banner.php';
+//include '../config/connection.php';
     ?>
 
       <link rel="stylesheet" type="text/css" href="../css/style.css" />
@@ -29,51 +30,57 @@
   <body>
       <?php echo $banner ?>
       <?php
-        include('../config/connection.php');
+        include '../config/connection.php';
 
- $con = mysql_connect("localhost", "root", "", "REUdata");
+ //$con = mysql_connect("localhost", "root", "", "REUdata");
 
-  if (!$con)
+  /*if (!$con)
   {
     die('Could not connect: ' . mysql_error());
-  };
+  };*/
     
-  mysql_select_db("REUdata", $con);
+  //mysql_select_db("REUdata", $db);
 
-        $title = $description = "";
+        $title = $description = $platform = $subject_area = "";
         
         if(isset($_POST['submit'])){
             //echo "submit button has been clicked";
                 if(empty($_POST['title']) || (empty($_POST['description']))){
-                //echo "one or more of the fields are empty";
+                //echo "You either forgot to put a course title or a course description.";
             }
             else{
                 $title = $_POST['title'];
                  $description = $_POST['description'];
+                $platform = $_POST['platform'];
+                $subject_area = $_POST['subject_area'];
                 
-                $query = "INSERT INTO courses (course_id, title, description, platform, subject_area, registration_code) VALUES (NULL, '$title', '$description', NULL, NULL, '54321')";
+                $query = "INSERT INTO courses (course_id, title, description, platform, subject_area, registration_code) VALUES (NULL, '$title', '$description', '$platform', '$subject_area', '54321')";
                 
-                $retval = mysql_query( $query, $con );
+                $retval = mysqli_query($db,$query);
     
     if(! $retval ) {
       die('Could not enter data: ' . mysql_error());
     }
     //echo "Entered data successfully\n";
 
-  mysql_close($con);       
+  //mysql_close($db);       
             }
     };
       
       ?>
       <div class="container main-page-form">
-          <h1>Add New Course</h1>
+          <h1>Add New Course</h1><br>
       <form  action="" method="POST" id ="add-course-form">
           <h4>Course Name</h4>
-          <input type="text" name="title" value="<?php echo $title;?>">
+          <input type="text" name="title" value="<?php echo $title;?>"><br>
+          <h4>Platform</h4>
+          <input type="text" name="platform" value="<?php echo $platform;?>"><br>
+          <h4>Subject Area</h4>
+          <input type="text" name="subject_area" value="<?php echo $subject_area;?>"><br><br>
 
           <h4>Short Course Description</h4>
-          <textarea rows="4" cols="50" id="course-description" name="description"><?php echo $description;?></textarea><br>
-      </form>
+          <textarea rows="4" cols="50" id="course-description" name="description"><?php echo $description;?></textarea>
+      </form><br>
           <h4>Course Attachments</h4>
            <form action="../php_scripts/upload.php" class="dropzone dz-clickable">
                <div class="dz-default dz-message">
