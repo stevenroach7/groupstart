31dae743ae5f7ebd73ba56dab947e30eff79b3a4
Tosin Alliyu <oballiyu@haverford.edu>
2016-07-26 14:53:05 -0500

    working on delete course

--- js/instructor-course.js
+++ js/instructor-course.js
@@ -1,14 +1,14 @@
 function removeButton(){
     console.log("button is clicked");
-    $('#submitDelete').attr('value', 'deleteCourse');
-    var currentDiv = $(this).closest('div[accordion]');
+    //$('#submitDelete').attr('value', 'deleteCourse');
+    //var currentDiv = $(this).closest('div[accordion]');
     
     var regCode = $('#registration-code').text();
     var inDex = regCode.indexOf(":")
     var forUrl = regCode.substring(inDex+2);
-    var removeActive = "?rA="+2+"&rC="+forUrl;
+    var removeActive = "?rA="+2;
     
-    document.location.hash = removeActive;
+    document.location.hash = document.location.hash+removeActive;
     //location.href = location.href + removeActive;
 };
 
