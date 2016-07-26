9fc0a0221a84f74712fadb73054df762c3d9beb1
Tosin Alliyu <oballiyu@haverford.edu>
2016-07-26 16:21:52 -0500

    Fixed js error

--- js/accordion_functionality.js
+++ js/accordion_functionality.js
@@ -4,10 +4,13 @@ $(document).ready(function() {
         collapsible: true,
         heightStyle: "content",
         activate: function (e) {
+                  //console.log($('.ui-accordion-header-active').text())
                 var activeTitle = $('.ui-accordion-header-active').text();
                 document.location.hash = "active="+activeTitle;
         }
       });
-});
+  
+    });
+
 
 
