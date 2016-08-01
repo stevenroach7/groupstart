5e4f80dc61180c325aa6800f50c1048a75fca995
Tosin Alliyu <oballiyu@haverford.edu>
2016-08-01 10:33:32 -0500

    added checkbox function to student start project

---
+++ js/student-start-project.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+        $('#check-description').click(function(){
+                $('#read-descrip').prop('checked', 'true');
+        });
+        
+        $('#check-statement').click(function(){
+                $('#read-statement').prop('checked', 'true');
+        });
+});
