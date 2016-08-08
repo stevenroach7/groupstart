9d18f2458699de86a720da882603a11f787c64ec
stevenroach7 <sroach@macalester.edu>
2016-08-08 16:17:45 -0500

    Edited display to make deliverable open accordion details more clear.

--- views/instructor-project.php
+++ views/instructor-project.php
@@ -268,7 +268,7 @@
                                                           <div class-'deliverable-title'>
                                                             <h4 class='panel-title'>
                                                                 $deliverable[title]
-                                                              <a data-toggle='collapse' data-parent='#accordion' href='#collapse$x' role='button' class='btn btn-default'>View Deliverable Details</a>
+                                                              <a data-toggle='collapse' data-parent='#accordion' href='#collapse$x' role='button' class='btn btn-default pull-right'>View Deliverable Details</a>
                                                             </h4>
                                                           </div>
                                                           <div class='date-display'>
