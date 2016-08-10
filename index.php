688a24859951d4f20b8dd5c047bc568cff3e8fd6
Tosin Alliyu <oballiyu@haverford.edu>
2016-08-10 16:48:41 -0500

    chanhed index styling

--- index.php
+++ index.php
@@ -49,35 +49,28 @@
 <body>
 
 
-  <nav class="navbar navbar-default navbar-fixed-top">
+  <nav class="navbar navbar-default navbar-fixed-top" style="background-color:#d3d3d3;border-style: outset;">
     <div class="container"><a class="banner pull-left" href="index.php" style="text-align:left"><h1>GroupStart</h1></a></div>
-  </nav>
+  </nav><br>
 
  <div class="jumbotron jumbotron-fluid" id="index-page" style="background-color: #d3d3d3 !important">
   <div class="container">
-    <div class="row" id="top-half">
-              <h1 style="text-align:center">Welcome to GroupStart</h1>
-              <div class="col-md-12">
                   <div class="row" id="webpage-description">
                       <div class="col-md-12">
-                            <p>
-      <b>GroupStart</b> is an experimental, alpha-stage application that seeks to facilitate the early stages of an online group project.
-    </p>
-                           <h5 style="text-align:center" id="research-affil">University of Minnesota Twin Cities - Computer Science &amp; Engineering - GroupLens Research</h5><br>
+                        <h1 style="text-align:center">Welcome to GroupStart</h1>
+                            <p> <b>GroupStart</b> is an experimental, alpha-stage application that seeks to facilitate the early stages of an online group project.</p><br>
+                           <h5 style="text-align:center" id="research-affil">University of Minnesota Twin Cities - Computer Science &amp; Engineering - GroupLens Research</h5>
                       </div>
                   </div>
                   <div class="row" id="web-icons-area">
-                      <div class="col-md-12">
+                    <div class="col-md-12">
                           <div id="web-rep-icons">
-                          <i class="fa fa-users fa-5x" aria-hidden="true" id="group-work">
-                          </i>
-                          <i class="fa fa-cloud fa-5x" aria-hidden="true" id="idea-generation"></i>
-                      <i class="fa fa-file-text-o fa-5x" aria-hidden="true" id="charter-formation"></i><br><h3>Group Work</h3><h3>Idea Formation</h3><h3>Charter Creation</h3></div>
-                  </div>
-
-              </div>
+                            <img src="img/multiple-users-silhouette.png" id="group-work"/>
+                            <img src="img/workers.png" id="group-formation"/>
+                            <img src="img/handshake.png" id="group-introduction"/><br>
+                            <h3 style="margin-right:55px;"></h3><h3 style="margin-right:35px;">Group Work</h3><h3 style="margin-right:15px;">Group Formation</h3><h3>Group Introduction</h3></div>
               </div></div>
-  </div>
+              </div>
      <a href="views/login.php" class="btn btn-info btn-lg center-block" role="button" id="get-started">Get Started</a>
 </div>
 
